cfae3c721f1284fccc2451a858685c3992bbab05
tehzz <tehzz@users.noreply.github.com>
2021-12-26 04:54:45 +0000

    Deploying to gh-pages from @ tehzz/bdsp-radar@120ed277637b3866ab65ddf77b87c5b6dcbc6543 🚀

---
+++ pkg/radarweb_bg.wasm.d.ts
@@ -0,0 +1,26 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function run_shiny_mc(a: number, b: number): void;
+export function run_shiny_mc_single_chain(a: number, b: number, c: number): void;
+export function __wbg_config_free(a: number): void;
+export function __wbg_get_config_chain_start(a: number): number;
+export function __wbg_set_config_chain_start(a: number, b: number): void;
+export function __wbg_get_config_chain_max(a: number): number;
+export function __wbg_set_config_chain_max(a: number, b: number): void;
+export function __wbg_get_config_sample_size(a: number): number;
+export function __wbg_set_config_sample_size(a: number, b: number): void;
+export function __wbg_get_config_total_shinies(a: number): number;
+export function __wbg_set_config_total_shinies(a: number, b: number): void;
+export function __wbg_get_config_pkmn_wildrate(a: number): number;
+export function __wbg_set_config_pkmn_wildrate(a: number, b: number): void;
+export function __wbg_get_config_time_for_catch(a: number): number;
+export function __wbg_set_config_time_for_catch(a: number, b: number): void;
+export function __wbg_get_config_time_for_run(a: number): number;
+export function __wbg_set_config_time_for_run(a: number, b: number): void;
+export function __wbg_get_config_time_for_reroll(a: number): number;
+export function __wbg_set_config_time_for_reroll(a: number, b: number): void;
+export function config_new_web(): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
